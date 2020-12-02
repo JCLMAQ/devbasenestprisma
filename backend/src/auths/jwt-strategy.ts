017b718383e5aac086fe4d12ad645007aad655a4
JCM <jcl.maquinay@gmail.com>
2020-12-02 15:18:37 +0100

    Jwt-strategy complement
    Test if JWT_LOGOUT_ENABLE

--- backend/src/auths/jwt-strategy.ts
+++ backend/src/auths/jwt-strategy.ts
@@ -4,12 +4,15 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 import { UsersService } from 'src/users/users.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
+  
   constructor(
     private usersService: UsersService,
-    private prismaService: PrismaService
+    private prismaService: PrismaService, 
+    private configService: ConfigService
     ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -27,16 +30,18 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     // Verify that the JWT payload is not cancel (even if the JWT is still valid - expiration time still OK)
     // The idea is that the logout action unvalid the API token
 
-    // TODO 
-    let tokenExist = await this.prismaService.token.findFirst({
-      where: {
-        userId: { equals: user.id },
-        type: { equals: "API" },
-      }
-    }) 
-    if(tokenExist) {
-      if(!tokenExist.valid){
-        throw new UnauthorizedException();
+    // Only if JWT LOGOUT enable
+    if(this.configService.get("JWT_LOGOUT_ENABLE") == 1) {
+      let tokenExist = await this.prismaService.token.findFirst({
+        where: {
+          userId: { equals: user.id },
+          type: { equals: "API" },
+        }
+      });
+      if(tokenExist) {
+        if(!tokenExist.valid){
+          throw new UnauthorizedException();
+        }
       }
     }
     return { userId: payload.sub, username: payload.username, role: user.Role };
