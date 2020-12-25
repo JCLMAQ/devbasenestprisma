423cd7c2d21528a239a6f53ee70a425a180bfafb
JCM <jcl.maquinay@gmail.com>
2020-12-25 23:36:16 +0100

    Start test users component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
+   { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
