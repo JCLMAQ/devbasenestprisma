cbd6dc4445e08643b008dbcfde66d14208212590
JCM <jcl.maquinay@gmail.com>
2020-12-28 11:11:43 +0100

    Trying to solve strick mode and mat table sort

--- frontend/src/app/user/user-list/user-list.component.ts
+++ frontend/src/app/user/user-list/user-list.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 import { User } from '../user.model';
 import { UserService } from '../user.service';
 
@@ -9,29 +14,51 @@ import { UserService } from '../user.service';
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
-  dataSource?: MatTableDataSource<User>;
-  tableColumns  :  string[] = [ 'nickName', 'lastName', 'firstName'] //, 'tools'];
+  dataSource!: MatTableDataSource<User>;
+  tableColumns  :  string[] = [ 'nickName', 'lastName', 'firstName', 'email'];
   users?: User[];
+  paginator?: MatPaginator;
+  sort?: MatSort
+
+  // @ViewChild(MatPaginator , { static: false }) paginator: MatPaginator;;
+
+  @ViewChild(MatPaginator, { static: true }) set matPaginator(mp: MatPaginator) {
+    if (mp !== undefined && this.dataSource) {
+      this.paginator = mp;
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+  @ViewChild(MatSort , { static: false }) set matSort(ms: MatSort) {
+    if (ms !== undefined && this.dataSource) {
+      this.sort = ms;
+      this.dataSource.sort = this.sort;
+    }
+  }
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private router: Router,
     ) {
+
       this.userService.getAllUsers().subscribe((objectResult) => {
         console.log('objectResult', objectResult);
         const arrayResult = Object.values(objectResult)
         console.log('arrayResult ', arrayResult)
         this.users = arrayResult;
-        this.dataSource  =  new MatTableDataSource(objectResult);
-        console.log('Datasource: ',this.dataSource)
-        })
-
+        console.log('users ', this.users);
+        this.dataSource  =  new MatTableDataSource(arrayResult);
+        console.log('Datasource: ',this.dataSource);
+        });
 
     }
 
   ngOnInit(): void {
-
   }
 
-  // users$ = this.userService.getAllUsers();
-
+  ngAfterViewInit() {
+    // private paginator: MatPaginator;
+    // private sort: MatSort;
+    // this.dataSource.paginator = this.paginator;
+  }
 
 }
+
--- frontend/src/app/user/user.module.ts
+++ frontend/src/app/user/user.module.ts
@@ -6,6 +6,8 @@ import { UserComponent } from './user.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     CommonModule,
     UserRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    SharedModule,
+    MatPaginatorModule
   ]
 })
 export class UserModule { }
