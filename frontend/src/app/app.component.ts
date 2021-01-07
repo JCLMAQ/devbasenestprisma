e6b784c02216636e789fd70e0b1fa96e576b3f36
JCM <jcl.maquinay@gmail.com>
2021-01-07 15:25:07 +0100

    Debbug

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {select, Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {distinctUntilChanged, map} from 'rxjs/operators';
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
+import { AppState } from './reducers';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  loading = true;
+
+  constructor(private router: Router,
+    private store: Store<AppState>) {
+  }
+
+ngOnInit(){
+// mat-spinner control base on navigation events
+  this.router.events.subscribe(event => {
+    switch (true) {
+        case event instanceof NavigationStart: {
+            this.loading = true;
+            break;
+        }
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+            this.loading = false;
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+  });
+}
+
 }
