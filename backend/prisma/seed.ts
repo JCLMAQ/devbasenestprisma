8e3f8d5bca5769d64df734a96d6745cdfd368a7e
JCM <jcl.maquinay@gmail.com>
2021-02-04 14:00:03 +0100

    chore

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -44,164 +44,128 @@ async function main() {
     },
   })
 
-  await prismaClient.user.create({
-    data: {
-      firstName: 'Moi',
-      lastName: 'MAQ',
-      email: 'jcm@jcm.be',
-      nickName: 'JCM',
-      Roles: "USER"
-    },
-  })
-  await prismaClient.user.create({
-    data: {
-      firstName: 'Toi',
-      lastName: 'TOIT',
-      email: 'toi@toit.be',
-      nickName: 'TOITOI',
-      Roles: "USER"
-    },
-  })
-  await prismaClient.user.create({
-    data: {
-      firstName: 'Toto',
-      lastName: 'THE BEST',
-      email: 'toto@toto.be',
-      nickName: 'TOTO',
-      Roles: "USER"
-    },
-  })
-  await prismaClient.user.create({
-    data: {
-      firstName: 'Best',
-      lastName: 'TOTO',
-      email: 'best@toto.be',
-      nickName: 'BTOTO',
-      Roles: "USER"
-    },
+  await prismaClient.user.createMany({
+    data: [
+      {
+        firstName: 'Moi',
+        lastName: 'MAQ',
+        email: 'jcm@jcm.be',
+        nickName: 'JCM',
+        Roles: "USER"
+      },
+      {
+        firstName: 'Toi',
+        lastName: 'TOIT',
+        email: 'toi@toit.be',
+        nickName: 'TOITOI',
+        Roles: "USER"
+      },
+      {
+        firstName: 'Toto',
+        lastName: 'THE BEST',
+        email: 'toto@toto.be',
+        nickName: 'TOTO',
+        Roles: "USER"
+      },
+      {
+        firstName: 'Best',
+        lastName: 'TOTO',
+        email: 'best@toto.be',
+        nickName: 'BTOTO',
+        Roles: "USER"
+      },
+    ]
   })
 
 
-  await prismaClient.configParam.create({
-    data: {
+  await prismaClient.configParam.createMany({
+    data: [
+      {
       name: 'EMAIL_DELAY_BTW_ENABLE',
       value: '1',
       utility: 'Activate limitation of asking email repeatedly.'
-    }
+      },
+      {
+        name: 'EMAIL_DELAY_BTW',
+        value: '10m',
+        utility: 'Delay between two email sending'
+      },
+      {
+        name: 'EMAIL_LIMIT_DOMAIN',
+        value: '1',
+        utility: 'Email domain limitation.'
+      },
+      {
+        name: 'EMAIL_HOST',
+        value: 'localhost',
+        utility: 'Email host domain.'
+      },
+      {
+        name: 'EMAIL_PORT',
+        value: '1025',
+        utility: 'Email host port.'
+      },
+      {
+        name: 'EMAIL_NOREPLY',
+        value: 'project.1@$localhost',
+        utility: 'No-reply email address.'
+      },
+      {
+        name: 'EMAIL_NOREPLY_PWD',
+        value: 'secret.1',
+        utility: 'No-reply email password.'
+      },
+      {
+        name: 'EMAIL_TOKEN_EXPIRATION',
+        value: '10m',
+        utility: 'Expiration delay for the email token.'
+      },
+      {
+        name: 'FORGOTPWD_TOKEN_EXPIRATION',
+        value: '10m',
+        utility: 'Expiration delay for the forgot paswword token.'
+      },
+      {
+        name: 'JWT_LOGOUT_ENABLE',
+        value: '1',
+        utility: 'Enable JWT cancelation when logout (! nedd an access to the DB each time, JWT is send to the backend).'
+      },
+      {
+        name: 'JWT_VALIDITY_DURATION',
+        value: '60s',
+        utility: 'Validity duration of the JWT token.'
+      },
+      {
+        name: 'FILES_STORAGE_DEST',
+        value: './uploadedfiles',
+        utility: '! Not used for now - URL for the files storage location.'
+      },
+      {
+        name: 'IMAGES_STORAGE_DEST',
+        value: './uploadedimages',
+        utility: " ! Not used for now - URL for the files storage location."
+      },
+      {
+        name: 'IMAGES_TEMP_STORAGE_DEST',
+        value: './uploadedtempimages',
+        utility: " ! Not used for now - URL for the files storage location."
+      }
+    ]
   })
-  await prismaClient.configParam.create({
-    data: {
-      name: 'EMAIL_DELAY_BTW',
-      value: '10m',
-      utility: 'Delay between two email sending'
-    }
-  })
-  await prismaClient.configParam.create({
-    data: {
-      name: 'EMAIL_LIMIT_DOMAIN',
-      value: '1',
-      utility: 'Email domain limitation.'
-    }
-  })
-  await prismaClient.configParam.create({
-    data: {
-      name: 'EMAIL_HOST',
-      value: 'localhost',
-      utility: 'Email host domain.'
-    }
-  })
-  await prismaClient.configParam.create({
-    data: {
-      name: 'EMAIL_PORT',
-      value: '1025',
-      utility: 'Email host port.'
-    }
-  })
-  await prismaClient.configParam.create({
-    data: {
-      name: 'EMAIL_NOREPLY',
-      value: 'project.1@$localhost',
-      utility: 'No-reply email address.'
-    }
-  })
-  await prismaClient.configParam.create({
-    data: {
-      name: 'EMAIL_NOREPLY_PWD',
-      value: 'secret.1',
-      utility: 'No-reply email password.'
-    }
-  })
-  await prismaClient.configParam.create({
-    data: {
-      name: 'EMAIL_TOKEN_EXPIRATION',
-      value: '10m',
-      utility: 'Expiration delay for the email token.'
-    }
-  })
-  await prismaClient.configParam.create({
-    data: {
-      name: 'FORGOTPWD_TOKEN_EXPIRATION',
-      value: '10m',
-      utility: 'Expiration delay for the forgot paswword token.'
-    }
-  })
-  await prismaClient.configParam.create({
-    data: {
-      name: 'JWT_LOGOUT_ENABLE',
-      value: '1',
-      utility: 'Enable JWT cancelation when logout (! nedd an access to the DB each time, JWT is send to the backend).'
-    }
-  })
-  await prismaClient.configParam.create({
-    data: {
-      name: 'JWT_VALIDITY_DURATION',
-      value: '60s',
-      utility: 'Validity duration of the JWT token.'
-    }
-  })
-
-  await prismaClient.configParam.create({
-    data: {
-      name: 'FILES_STORAGE_DEST',
-      value: './uploadedfiles',
-      utility: '! Not used for now - URL for the files storage location.'
-    }
-  })
-
-  await prismaClient.configParam.create({
-    data: {
-      name: 'IMAGES_STORAGE_DEST',
-      value: './uploadedimages',
-      utility: " ! Not used for now - URL for the files storage location."
-    }
-  })
-
-  await prismaClient.configParam.create({
-    data: {
-      name: 'IMAGES_TEMP_STORAGE_DEST',
-      value: './uploadedtempimages',
-      utility: " ! Not used for now - URL for the files storage location."
-    }
-  })
-
-
-  await prismaClient.emaildomain.create({
-    data: {
+  await prismaClient.emaildomain.createMany({
+    data:[ {
       domain: 'test.be',
       allowed: true
-    }
-  })
-  await prismaClient.emaildomain.create({
-    data: {
-      domain: 'gmail.com',
-      allowed: true
-    }
-  })
-  await prismaClient.emaildomain.create({
-    data: {
-      domain: 'outlook.be',
-      allowed: false
-    }
+      },
+      {
+        domain: 'gmail.com',
+        allowed: true
+      },
+      {
+        domain: 'outlook.be',
+        allowed: false
+      }
+    ]
   })
 }
 
