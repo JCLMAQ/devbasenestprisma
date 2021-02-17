ae9af721344fa95ccb0c05adc9d8c24d76cec19f
JCM <jcl.maquinay@gmail.com>
2021-02-17 16:02:18 +0100

    Debug of prisma seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,16 +1,32 @@
 import { users } from './users';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Role } from '@prisma/client';
+import  faker  from 'faker';
 
 const prisma = new PrismaClient();
 
-async function main() {
-    for ( let user of users) {
-        await prisma.user.create({
-            data: user
-        })
-    }
-}
 
+// async function main() {
+//     for ( let user of users) {
+//         await prisma.user.create({
+//             data: user
+//         })
+//     }
+// }
+
+const data = Array.from({ length: 10 }).map(()=> ({
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    Roles: Role.USER
+
+}))
+
+// const data = Array.from( users );
+async function main() {
+   await prisma.user.createMany({
+            data
+        })
+}
 main().catch(e => {
     console.log(e);
     process.exit(1);
--- backend/prisma/users.ts
+++ backend/prisma/users.ts
@@ -1,31 +1,33 @@
+import { Role } from "@prisma/client";
+
 export const users = [
     {
         firstName: 'Moi',
         lastName: 'MAQ',
         email: 'jcm@jcm.be',
         nickName: 'JCM',
-        Roles: "User"
+        Roles: Role.USER
     },
     {
         firstName: 'Toi',
         lastName: 'TOIT',
         email: 'toi@toit.be',
         nickName: 'TOITOI',
-        Roles: "User"
+        Roles: Role.USER
     },
     {
         firstName: 'Toto',
         lastName: 'THE BEST',
         email: 'toto@toto.be',
         nickName: 'TOTO',
-        Roles: "User"
+        Roles: Role.USER
     },
     {
         firstName: 'Best',
         lastName: 'TOTO',
         email: 'best@toto.be',
         nickName: 'BTOTO',
-        Roles: "User"
+        Roles: Role.USER
     },
 ]
 
