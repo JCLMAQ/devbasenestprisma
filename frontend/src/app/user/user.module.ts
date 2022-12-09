5b35ff9c95c7d0aee9dcf21aa2048075b2888551
JCM <jcl.maquinay@gmail.com>
2022-12-09 10:31:44 +0100

    div

--- frontend/src/app/user/user.module.ts
+++ frontend/src/app/user/user.module.ts
@@ -1,22 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { UserRoutingModule } from './user-routing.module';
-import { UserComponent } from './user.component';
-import { UserListComponent } from './user-list/user-list.component';
 import { HttpClientModule } from '@angular/common/http';
-import { SharedModule } from '../shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveComponentModule } from '@ngrx/component';
 import { EntityDataService, EntityDefinitionService, EntityMetadataMap } from '@ngrx/data';
+import { SharedModule } from '../shared/shared.module';
+import { UserDataService } from './store/user-data.service';
 import { UserEntityService } from './store/user-entity.service';
 import { UserResolver } from './store/user.resolver';
-import { UserDataService } from './store/user-data.service';
+import { UserListComponent } from './user-list/user-list.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import { ReactiveComponentModule } from '@ngrx/component';
+import { UserRoutingModule } from './user-routing.module';
+import { UserComponent } from './user.component';
 
 const entityMetadata: EntityMetadataMap = {
   User: {
-
   },
 };
 
